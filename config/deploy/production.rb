cea229e6513a42aba38cc2afd8f0c403168cb65f
Daniil Guzanov <melkor217@gmail.com>
2016-05-07 07:17:05 +0300

    Lil fixes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -73,7 +73,7 @@ set :docker_compose_project_name, 'test'
 
 set :docker_detach, false
 set :docker_compose, true
-set :docker_compose_build_services, 'ayanami'
+set :docker_compose_build_services, 'ayanami-production'
 set :docker_compose_remove_after_stop, false
 set :docker_compose_project_name, 'ayanami'
 
