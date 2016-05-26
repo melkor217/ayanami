d88b0825d75ca9f43efec53bad2c34d26da2173e
Daniil Guzanov <melkor217@gmail.com>
2016-05-26 20:33:57 +0300

    Livestats model

---
+++ app/models/livestats.rb
@@ -0,0 +1,3 @@
+class Livestats < ApplicationRecord
+  self.table_name = 'hlstats_Livestats' # MySQL table name
+end
---
+++ test/models/livestats_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LivestatsTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
