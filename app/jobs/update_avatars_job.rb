04a3a3d508d5d403e47762e22eca37ce22e47a16
Daniil Guzanov <melkor217@gmail.com>
2016-06-10 21:34:14 +0300

    Descrease batch side for UpdateAvatars
    Well, we need semaphores for steam requests

--- app/jobs/update_avatars_job.rb
+++ app/jobs/update_avatars_job.rb
@@ -5,7 +5,7 @@ class UpdateAvatarsJob < ApplicationJob
     UniqueId.where(steamUpdated: nil).order(playerId: :desc).limit(300).each do |id|
       GetPlayerSteamInfoJob.perform_later(game: id.game, uniqueId: id.uniqueId)
     end
-    UniqueId.where('steamUpdated < ?', 3.days.ago).limit(100).each do |id|
+    UniqueId.where('steamUpdated < ?', 3.days.ago).limit(30).each do |id|
       GetPlayerSteamInfoJob.perform_later(game: id.game, uniqueId: id.uniqueId)
     end
   end
