54824d14a0102145a81511e11a326d12cc944639
Daniil Guzanov <melkor217@gmail.com>
2016-06-10 20:37:53 +0300

    Fix migrations

--- db/migrate/20160601180030_add_option_steam_group.rb
+++ db/migrate/20160601180030_add_option_steam_group.rb
@@ -1,5 +1,8 @@
 class AddOptionSteamGroup < ActiveRecord::Migration[5.0]
-  def change
+  def up
     Option.create(keyname: 'steamgroup', value: 'reijii-dm', opttype: 2)
+  end
+  def down
+    Option.where(keyname: 'steamgroup').delete_all
   end
 end
