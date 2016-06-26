b5d6fc38dfbdbae9ed88251496915a224e976400
Daniil Guzanov <melkor217@gmail.com>
2016-06-27 00:21:05 +0300

    Better semaphore and logging for GetPlayerSteamInfo

--- app/jobs/get_player_steam_info_job.rb
+++ app/jobs/get_player_steam_info_job.rb
@@ -1,15 +1,22 @@
 class GetPlayerSteamInfoJob < ApplicationJob
   def perform(options = {})
-    logger.warn options.to_s
+    logger.debug "GetPlayerSteamInfo with #{options.to_s}"
     uniqueid = UniqueId.find_by!(options)
     # Do something later
     if uniqueid
       s = Redis::Semaphore.new(:steam, Rails.application.config.semaphore)
-      s.lock
+      logger.debug("#{@job_id} Locking: start")
+      s.lock(60)
+      logger.debug("#{@job_id} Locking: done")
       begin
         if uri = SteamId.steam_profile_url(uniqueid.uniqueId, format: :xml)
-          doc = Nokogiri::XML(Net::HTTP.get(uri))
-          logger.debug("Getting info for player #{uri.to_s}")
+          logger.debug("#{@job_id} Getting info for player #{uri.to_s}")
+          body = Net::HTTP.start(uri.host, uri.port, read_timeout: 15) do |http|
+            request = http.request(Net::HTTP::Get.new(uri))
+            (request.kind_of? Net::HTTPSuccess) ? request.body : nil
+          end
+          logger.debug("#{@job_id} Done")
+          doc = Nokogiri::XML(body)
           raise IOError, 'Incorrect XML :(' if doc.xpath('//profile/steamID64').text.to_i == 0
 
           uniqueid.avatarFull = doc.xpath('//profile/avatarFull').text
@@ -27,11 +34,12 @@ class GetPlayerSteamInfoJob < ApplicationJob
         uniqueid.steamUpdated = Time.now
         uniqueid.save
       rescue
-        logger.warn("Failed to get steam profile for #{id64}")
+        logger.warn("#{@job_id} Failed to get steam profile for #{uniqueid.uniqueId}")
         sleep 5
         s.unlock
         raise
       ensure
+        logger.debug "#{@job_id} Finishing.. #{uri.to_s}"
         s.unlock
       end
     end
