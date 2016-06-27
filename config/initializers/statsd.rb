b042bb2e8a0c40451918fc38bfa32d599c7558bf
Daniil Guzanov <melkor217@gmail.com>
2016-06-28 01:13:29 +0300

    Better graphite initializer

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -1,9 +1,13 @@
 require 'trashed/railtie'
 require 'statsd'
-
+begin
+  $statsd = Statsd.new('graphite')
+rescue SocketError
+  $statsd = Statsd.new('127.0.0.1')
+end
 
 if ENV['STATSD_PROFILER'] and ENV['STATSD_PROFILER'].to_s.downcase == 'true'
-  Ayanami::Application.config.trashed.statsd = Statsd.new('graphite')
+  Ayanami::Application.config.trashed.statsd = $statsd
   Ayanami::Application.config.trashed.timing_dimensions = ->(env) do
     # Rails 3 and 4 set this. Other Rack endpoints won't have it.
     if (controller = env['action_controller.instance'])
